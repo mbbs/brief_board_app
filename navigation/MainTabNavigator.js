e16171f8c48fdaba6fdc681942207f5e212948c8
mshah <shahmaulikb@gmail.com>
2020-02-23 21:46:54 -0800

    adding sigin

--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -1,40 +1,60 @@
 import React from 'react';
-import { Platform } from 'react-native';
-import { createStackNavigator } from 'react-navigation-stack';
-import { createBottomTabNavigator } from 'react-navigation-tabs';
+import {Platform} from 'react-native';
+import {createStackNavigator} from 'react-navigation-stack';
+import {createBottomTabNavigator} from 'react-navigation-tabs';
 
 import TabBarIcon from '../components/TabBarIcon';
 import HomeScreen from '../screens/HomeScreen';
+import LoginScreen from '../screens/LoginScreen';
 import LinksScreen from '../screens/LinksScreen';
 import SettingsScreen from '../screens/SettingsScreen';
 
 const config = Platform.select({
-  web: { headerMode: 'screen' },
-  default: {},
+    web: {headerMode: 'screen'},
+    default: {},
 });
 
 const HomeStack = createStackNavigator(
+    {
+        Home: HomeScreen,
+        Login: LoginScreen
+    },
+    config
+);
+
+HomeStack.navigationOptions = {
+    tabBarLabel: 'Home',
+    tabBarIcon: ({focused}) => (
+        <TabBarIcon
+            focused={focused}
+            name={
+                Platform.OS === 'ios'
+                    ? `ios-information-circle${focused ? '' : '-outline'}`
+                    : 'md-information-circle'
+            }
+        />
+    ),
+};
+
+HomeStack.path = '';
+
+
+const LoginStack = createStackNavigator(
   {
-    Home: HomeScreen,
+    Login: LoginScreen,
   },
   config
 );
 
-HomeStack.navigationOptions = {
-  tabBarLabel: 'Home',
+LoginStack.navigationOptions = {
+  tabBarLabel: 'Login',
   tabBarIcon: ({ focused }) => (
-    <TabBarIcon
-      focused={focused}
-      name={
-        Platform.OS === 'ios'
-          ? `ios-information-circle${focused ? '' : '-outline'}`
-          : 'md-information-circle'
-      }
-    />
+    <TabBarIcon focused={focused} name={Platform.OS === 'ios' ? 'ios-link' : 'md-link'} />
   ),
 };
 
-HomeStack.path = '';
+LoginStack.path = '';
+
 //
 // const LinksStack = createStackNavigator(
 //   {
@@ -69,9 +89,10 @@ HomeStack.path = '';
 // SettingsStack.path = '';
 
 const tabNavigator = createBottomTabNavigator({
-  HomeStack
-  // LinksStack,
-  // SettingsStack,
+    HomeStack,
+    LoginStack
+    // LinksStack,
+    // SettingsStack,
 });
 
 tabNavigator.path = '';
---
+++ screens/LoginScreen.js
@@ -0,0 +1,230 @@
+// import { GoogleSignin, statusCodes, GoogleSigninButton } from '@react-native-community/google-signin';
+import * as Google from 'expo-google-app-auth';
+import React, {Component} from 'react';
+import {Button, StyleSheet, Text, View, Image, TouchableOpacity} from 'react-native';
+import firebase from "firebase";
+import {AsyncStorage} from "react-native";
+import GoogleLogo from '../assets/images/google-signin.png';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCopduK8cTcaM5e9iM5nz9qwZBA7LsV9Tc",
+    authDomain: "fresh-sanctuary-241303.firebaseapp.com",
+    databaseURL: "https://fresh-sanctuary-241303.firebaseio.com",
+    projectId: "fresh-sanctuary-241303",
+    storageBucket: "fresh-sanctuary-241303.appspot.com",
+    messagingSenderId: "689009111160",
+    appId: "1:689009111160:web:b7bb1cd917b29d60c03d8d",
+    measurementId: "G-P3DV4ER3P4"
+};
+if (!firebase.apps.length) {
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+}
+
+
+const config = {
+    iosClientId: '689009111160-0empa8quf6m2bm0s0006v5v6mcg431a6.apps.googleusercontent.com',
+    scopes: ["profile", "email"]
+};
+
+export default class LoginScreen extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isSignedIn: false,
+            name: undefined,
+            photoUrl: undefined
+        };
+    }
+
+    componentDidMount() {
+        this.loadUser();
+    }
+
+    loadUser = async () => {
+        const userName = await AsyncStorage.getItem('@VidBrief:userName');
+        const photoUrl = await AsyncStorage.getItem('@VidBrief:photoUrl');
+        if (userName) {
+            this.stateSetHelper(userName, photoUrl);
+        }
+    };
+
+    signOut = async () => {
+        await firebase.auth().signOut();
+        await AsyncStorage.removeItem('@VidBrief:userName');
+        await AsyncStorage.removeItem('@VidBrief:photoUrl');
+        await this.setState({
+            isSignedIn: false,
+            name: undefined,
+            photoUrl: undefined
+        });
+    };
+
+    stateSetHelper = async (name, photoUrl) => {
+        try {
+            await AsyncStorage.setItem('@VidBrief:userName', name);
+            await AsyncStorage.setItem('@VidBrief:photoUrl', photoUrl);
+            await this.setState({
+                isSignedIn: true,
+                name: name,
+                photoUrl: photoUrl
+            });
+        } catch (error) {
+            console.log(error)
+            // Error saving data
+        }
+    };
+
+    onSignIn = (googleUser) => {
+        console.log('Google Auth Response', googleUser);
+        // We need to register an Observer on Firebase Auth to make sure auth is initialized.
+        const that = this;
+        const unsubscribe = firebase.auth().onAuthStateChanged(function (firebaseUser) {
+            unsubscribe();
+            // Check if we are already signed-in Firebase with the correct user.
+            if (!that.isUserEqual(googleUser, firebaseUser)) {
+                // Build Firebase credential with the Google ID token.
+                const credential = firebase.auth.GoogleAuthProvider.credential(
+                    googleUser.idToken);
+
+                // Sign in with credential from the Google user.
+                firebase.auth().signInWithCredential(credential)
+                    .then(() => that.stateSetHelper(googleUser.user.name, googleUser.user.photoUrl))
+                    .catch(function (error) {
+                        console.error(error);
+                    });
+            } else {
+                that.stateSetHelper(googleUser.user.name, googleUser.user.photoUrl)
+                console.log('User already signed-in Firebase.');
+            }
+        });
+    };
+
+    isUserEqual = (googleUser, firebaseUser) => {
+        if (firebaseUser) {
+            var providerData = firebaseUser.providerData;
+            for (var i = 0; i < providerData.length; i++) {
+                if (providerData[i].providerId === firebase.auth.GoogleAuthProvider.PROVIDER_ID &&
+                    providerData[i].uid === googleUser.user.id) {
+                    // We don't need to reauth the Firebase connection.
+                    return true;
+                }
+            }
+        }
+        return false;
+    };
+
+    signIn = async () => {
+        try {
+            const result = await Google.logInAsync(config);
+            this.onSignIn(result);
+        } catch (e) {
+            return {error: true};
+        }
+    };
+
+    render() {
+        if (this.state.isSignedIn) {
+            return <View style={styles.container}>
+                <View style={styles.imageView}>
+                    <Image
+                        style={{width: 200, height: 200, borderRadius: 100}}
+                        source={{uri: this.state.photoUrl}}
+                    />
+                </View>
+                <View style={styles.welcomeTextView}>
+                    <Text style={styles.welcomeText}>
+                        <Text>Hi, </Text>
+                        <Text style={styles.welcomeTextName}>{this.state.name}</Text>
+                    </Text>
+                    <Text style={styles.welcomeText}>Welcome to VidBrief!</Text>
+                </View>
+                <TouchableOpacity style={styles.signOutButton} onPress={this.signOut}>
+                    <Text style={styles.signOutButtonText}>Sign out</Text>
+                </TouchableOpacity>
+            </View>
+        } else {
+            return <View style={styles.signInView}>
+                <Text style={styles.signInWithHeading}>Sign In</Text>
+                <View
+                    style={{
+                        borderWidth: 1,
+                        borderColor:'#000',
+                        width: "80%",
+                        margin:20,
+                    }}
+                />
+            <TouchableOpacity onPress={this.signIn} style={styles.googlePlusStyle}>
+                <Image style={{width: 60, height: 60}} source={require('../assets/images/google-signin.png')}/>
+                <Text style={styles.signInButtonText}>Sign In with Google</Text>
+            </TouchableOpacity>
+            </View>
+        }
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        fontSize: 15,
+        fontFamily: 'System',
+    },
+    imageView: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#fff',
+        height: '50%'
+    },
+    welcomeTextView: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        paddingTop: 12
+    },
+    welcomeText: {
+        fontSize: 35,
+        paddingBottom: 10
+    },
+    welcomeTextName: {
+        fontWeight: 'bold'
+    },
+    signOutButton: {
+        marginTop: 25,
+        backgroundColor: '#f5f5f5',
+        borderColor: '#f5f5f5',
+        borderWidth: 15,
+        fontSize: 35,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    signOutButtonText: {
+        fontSize: 25,
+        color: "#FF0000"
+    },
+    signInView: {
+        flex: 1,
+        backgroundColor: '#fff',
+        fontSize: 15,
+        fontFamily: 'System',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    googlePlusStyle: {
+        flexDirection: 'row',
+        borderWidth: 2,
+        backgroundColor: '#4c8bf5',
+        borderColor: '#f5f5f5',
+        height: 60,
+        fontSize: 35,
+        width: "80%",
+        alignItems: 'center'
+    },
+    signInButtonText: {
+        fontSize: 20,
+        color: '#fff',
+        paddingLeft: 20
+    },
+    signInWithHeading: {
+        fontSize: 25
+    }
+});
