33e80d56010a060189f7f0406236b099617a04b7
Niyati Shah <niyati@Niyatis-MacBook-Air.local>
2020-03-12 21:47:08 -0700

    changes

--- screens/FeedbackScreen.js
+++ screens/FeedbackScreen.js
@@ -1,8 +1,40 @@
 import React, {Component} from 'react';
 import {WebView} from 'react-native-webview';
+import { AppLoading, SplashScreen } from 'expo';
+import {StyleSheet, Text, View, Image, TouchableOpacity, ActivityIndicator} from 'react-native';
 
 export default class FeedbackScreen extends Component {
+	
+	constructor(props) {
+		super(props);
+		this.state
+	}
+
+	componentDidMount() {
+    	SplashScreen.preventAutoHide();
+    }
+
+    indicator() {
+		return (<View style={styles.indicator}>
+			<ActivityIndicator size="large" color="#000" />
+			</View>);
+	}
+
+
     render() {
-        return <WebView source={{uri: "https://forms.gle/hxpWFVzZMijQkGQ58"}}/>
+        return <WebView source={{uri: "https://forms.gle/hxpWFVzZMijQkGQ58"}}
+        startInLoadingState={true}
+        style={styles.webview}
+        renderLoading={this.indicator} 
+        />
     }
 }
+
+const styles = StyleSheet.create({
+  indicator: {
+  	flex: 1
+  },
+  webview: {
+		flex: 1,
+	},
+});
